89ce8404eae292342f0f2c076bcb90df346dfd5f
daoern <noreply@daoern.com>
2023-11-07 23:11:43 +0800

    develop: added redux state management

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import getRouteMapping from "./route/RouteMapping";
 import "./common/i18n/config";
+import { Provider } from "react-redux";
+import { appStore } from "./stores/appStore";
 
 const router = createBrowserRouter(getRouteMapping());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={appStore}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
